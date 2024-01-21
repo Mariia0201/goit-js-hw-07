c60f760eb51421637eebc7ef1d9cd7079d37d705
Mariia <mariadufanets06@gmail.com>
2024-01-21 18:34:09 +0200

    update

--- js/task-3.js
+++ js/task-3.js
@@ -2,11 +2,11 @@ const inputContent = document.querySelector('#name-input');
 const outputContent = document.querySelector('#name-output')
 function checkName() {
     const inputValue = inputContent.value.trim();
-    if (inputContent === '') {
-        outputContent.textContent === 'Anonymous';
+    if (inputValue === '') {
+        outputContent.textContent = 'Anonymous';
     }
     else {
-        return outputContent.textContent = inputValue;
+        outputContent.textContent = inputValue;
     }
 }
 
