527bedd2281f6229c042c8acdd12591844e206c6
Mariia <mariadufanets06@gmail.com>
2024-01-22 19:27:21 +0200

    update

--- js/task-6.js
+++ js/task-6.js
@@ -1,35 +1,39 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
 }
 
 const input = document.querySelector('input');
-  const createButton = document.querySelector('[data-create]');
-  const destroyButton = document.querySelector('[data-destroy]');
-  const boxesContainer = document.getElementById('boxes');
+const createButton = document.querySelector('[data-create]');
+const destroyButton = document.querySelector('[data-destroy]');
+const boxesContainer = document.getElementById('boxes');
 
-  function createBoxes(amount) {
-    for (let i = 0; i < amount; i++) {
-      const box = document.createElement('div');
-      box.style.width = `${30 + i * 10}px`;
-      box.style.height = `${30 + i * 10}px`;
-      box.style.backgroundColor = getRandomHexColor();
-      boxesContainer.appendChild(box);
-    }
+function createBoxes(amount) {
+  
+  boxesContainer.innerHTML = '';
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.width = `${30 + i * 10}px`;
+    box.style.height = `${30 + i * 10}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesContainer.appendChild(box);
   }
+}
 
-  function destroyBoxes() {
-    boxesContainer.innerHTML = '';
+function destroyBoxes() {
+  
+  boxesContainer.innerHTML = '';
+}
+
+createButton.addEventListener('click', function () {
+  const amount = input.valueAsNumber;
+
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    input.value = '';
+  } else {
+    alert('Please enter a number between 1 and 100.');
   }
+});
 
-  createButton.addEventListener('click', function () {
-    const amount = input.valueAsNumber;
-
-    if (amount >= 1 && amount <= 100) {
-      createBoxes(amount);
-      input.value = '';
-    } else {
-      alert('Please enter a number between 1 and 100.');
-    }
-  })
+destroyButton.addEventListener('click', destroyBoxes);
